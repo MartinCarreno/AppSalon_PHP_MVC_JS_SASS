904b2bdc3e80c7be21ddd7fd0b4186c4928501d6
Martin Carreño <martincarreno71@gmail.com>
2026-05-09 04:36:48 -0400

    script db
    El script que contiene la creacion de las tablas para poder trabajar mas
    rapido si hay que utilizar otttro pc.

--- docs/script.sql
+++ docs/script.sql
@@ -1,10 +1,59 @@
 -- tabla usuarios
-ALTER TABLE appsalon_mvc.usuarios ADD nombre varchar(60) NULL;
-ALTER TABLE appsalon_mvc.usuarios ADD apellido varchar(60) NULL;
-ALTER TABLE appsalon_mvc.usuarios ADD email varchar(40) NULL;
-ALTER TABLE appsalon_mvc.usuarios ADD telefono varchar(10) NULL;
-ALTER TABLE appsalon_mvc.usuarios ADD admin TINYINT(1) NULL;
-ALTER TABLE appsalon_mvc.usuarios ADD confirmado TINYINT(1) NULL;
-ALTER TABLE appsalon_mvc.usuarios ADD token varchar(15) NULL;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `apellido` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(40) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `telefono` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `admin` tinyint(1) DEFAULT NULL,
+  `confirmado` tinyint(1) DEFAULT NULL,
+  `token` varchar(15) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
 
 -- tabla servicios
+
+CREATE TABLE `servicios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nombre` varchar(60) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `precio` decimal(5,2) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+--inserts
+INSERT INTO `servicios` (`id`, `nombre`, `precio`) VALUES
+(1, 'Corte de Cabello Mujer', 90.00),
+(2, 'Corte de Cabello Hombre', 80.00),
+(3, 'Corte de Cabello Niño', 60.00),
+(4, 'Peinado Mujer', 80.00),
+(5, 'Peinado Hombre', 60.00),
+(6, 'Peinado Niño', 60.00),
+(7, 'Corte de Barba', 60.00),
+(8, 'Tinte de Mujer', 300.00),
+(9, 'Uñas', 400.00),
+(10, 'Lavado de Cabello', 50.00),
+(11, 'Tratamiento Capilar', 150.00);
+
+--Tabla Citas
+
+CREATE TABLE `citas` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fecha` date DEFAULT NULL,
+  `hora` time DEFAULT NULL,
+  `usuarioId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citas_usuarios_FK` (`usuarioId`),
+  CONSTRAINT `citas_usuarios_FK` FOREIGN KEY (`usuarioId`) REFERENCES `usuarios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--Tabla citasServicios
+CREATE TABLE `citasServicio` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `citaId` int DEFAULT NULL,
+  `servicioId` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `citasServicio_citas_FK` (`citaId`),
+  KEY `citasServicio_servicios_FK` (`servicioId`),
+  CONSTRAINT `citasServicio_citas_FK` FOREIGN KEY (`citaId`) REFERENCES `citas` (`id`),
+  CONSTRAINT `citasServicio_servicios_FK` FOREIGN KEY (`servicioId`) REFERENCES `servicios` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
