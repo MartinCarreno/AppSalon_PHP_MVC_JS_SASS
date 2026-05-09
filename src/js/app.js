2880184d21b017cbb12d04647f4fa99681361fbf
Martin Carreño <martincarreno71@gmail.com>
2026-05-09 19:21:19 -0400

    Funcion para extraer nombre
    creada la funcion nombreCliente() para agregar el nombre al objeto cita

--- src/js/app.js
+++ src/js/app.js
@@ -21,6 +21,7 @@ function iniciarApp() {
   paginaAnterior();
 
   consultarAPI(); // Consulta la api en el backend de php
+  nombreCliente(); //Añade el nombre del cliente para el objeto cita
 }
 
 function mostrarSeccion() {
@@ -154,4 +155,8 @@ function seleccionarServicio(servicio) {
   }
 
   console.log(cita);
+}
+
+function nombreCliente() {
+  cita.nombre = document.querySelector("#nombre").value;
 }
