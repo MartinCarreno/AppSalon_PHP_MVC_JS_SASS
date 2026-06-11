00ee969cda37dcecc5678b3339516641cadf382f
Martin Carreno <martincarreno71@gmail.com>
2026-06-11 01:49:14 -0400

    Consulta Agregada
    Se agregó la consulta al admin controller, asi como tambien se modifico el modelo Active Record para  realizar consultas planas.

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -1,12 +1,29 @@
 <?php
 
 namespace Controllers;
+
+use Model\AdminCita;
 use MVC\Router;
 
 class AdminController
 {
     public static function index( Router $router ){
         session_start();
+
+        //consultar DB
+        $consulta = "SELECT citas.id, citas.hora, CONCAT( usuarios.nombre, ' ', usuarios.apellido) as cliente, ";
+        $consulta .= " usuarios.email, usuarios.telefono, servicios.nombre as servicio, servicios.precio  ";
+        $consulta .= " FROM citas  ";
+        $consulta .= " LEFT OUTER JOIN usuarios ";
+        $consulta .= " ON citas.usuarioId=usuarios.id  ";
+        $consulta .= " LEFT OUTER JOIN citaServicios ";
+        $consulta .= " ON citaServicios.citaId=citas.id ";
+        $consulta .= " LEFT OUTER JOIN servicios ";
+        $consulta .= " ON servicios.id=citaServicios.servicioId ";
+        //$consulta .= " WHERE fecha =  '$fecha' ";
+
+        $citas = AdminCita::SQL($consulta);
+        debuguear($citas);
 
         $router->render('admin/index', [
             'nombre' => $_SESSION['nombre']
